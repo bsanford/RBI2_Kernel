612689b93138e50fa9a4f92d94bd503ff9b9c3af
bbot <sanford.braeden@gmail.com>
2016-10-11 11:19:02 -0400

    remove GPIO/gpio.c

---
+++ GPIO/gpio.h
@@ -0,0 +1,65 @@
+#ifndef GPIO_H_INCLUDED
+#define GPIO_H_INCLUDED
+
+#define GPIO_BASE       0x3F200000UL
+
+
+
+#define LED_GPFSEL      GPIO_GPFSEL1
+#define LED_GPFBIT      24
+#define LED_GPSET       GPIO_GPSET0
+#define LED_GPCLR       GPIO_GPCLR0
+#define LED_GPIO_BIT    18
+
+
+
+
+#define GPIO_GPFSEL0    0
+#define GPIO_GPFSEL1    1
+#define GPIO_GPFSEL2    2
+#define GPIO_GPFSEL3    3
+#define GPIO_GPFSEL4    4
+#define GPIO_GPFSEL5    5
+
+#define GPIO_GPSET0     7
+#define GPIO_GPSET1     8
+
+#define GPIO_GPCLR0     10
+#define GPIO_GPCLR1     11
+
+#define GPIO_GPLEV0     13
+#define GPIO_GPLEV1     14
+
+#define GPIO_GPEDS0     16
+#define GPIO_GPEDS1     17
+
+#define GPIO_GPREN0     19
+#define GPIO_GPREN1     20
+
+#define GPIO_GPFEN0     22
+#define GPIO_GPFEN1     23
+
+#define GPIO_GPHEN0     25
+#define GPIO_GPHEN1     26
+
+#define GPIO_GPLEN0     28
+#define GPIO_GPLEN1     29
+
+#define GPIO_GPAREN0    31
+#define GPIO_GPAREN1    32
+
+#define GPIO_GPAFEN0    34
+#define GPIO_GPAFEN1    35
+
+#define GPIO_GPPUD      37
+#define GPIO_GPPUDCLK0  38
+#define GPIO_GPPUDCLK1  39
+
+struct gpio_pin{
+volatile unsigned int p_nmb; /*The GPIO Pin number*/
+volatile unsigned int fnc_slt; /*Its associated function select*/
+unsigned int mtex;             /*mutex lock*/
+};
+
+
+#endif // GPIO_H_INCLUDED
---
+++ init_kernel.c
@@ -0,0 +1,61 @@
+#include "GPIO/gpio.h"
+
+
+/*Func: Kernel_Main
+ * 
+ * ARGS: r0, r1 and atags. TODO get definitions from arm processor manual
+ * 
+ * Modified from the baremetal tutorials on Valvers.com
+ *
+ * Entry point for the kernel image, 
+ * Initializes the GPIO pin and sets it to on waits for some time
+ * Sets it to off and waits for some time. 
+ */
+
+/* GPIO Register set */
+volatile unsigned int* gpio;
+
+/** Simple loop variable */
+volatile unsigned int tim;
+
+void kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
+{
+
+    struct gpio_pin pin_set[52]; /*Holds the set of GPIO Pins*/
+
+
+    /*Initialize the gpio pins based on location and*/
+
+    /* Assign the address of the GPIO peripheral (Using ARM Physical Address) */
+    gpio = (unsigned int*)GPIO_BASE;
+
+    /* Write 1 to the GPIO16 init nibble in the Function Select 1 GPIO
+       peripheral register to enable GPIO16 as an output */
+    gpio[LED_GPFSEL] |= (1 << LED_GPFBIT);
+
+    /* Never exit as there is no OS to exit to! */
+    while(1)
+    {
+        for(tim = 0; tim < 500000; tim++)
+            ;
+
+        /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off
+           for plus models )*/
+        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+
+        for(tim = 0; tim < 500000; tim++)
+            ;
+
+        /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on
+           for plus models )*/
+        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+    }
+}
+
+
+
+
+
+
+
+
