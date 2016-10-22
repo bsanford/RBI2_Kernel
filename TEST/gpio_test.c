7879e41578e807c6733bf377970681b882d47696
bbot <sanford.braeden@gmail.com>
2016-10-22 12:27:19 -0400

    added a sleep function using system clock

--- TEST/gpio_test.c
+++ TEST/gpio_test.c
@@ -1,7 +1,18 @@
 #include "gpio_api.h"
+#include "sys_time.h"
 
 
 /*Test the gpio functionality using the api */
+
+
+/*Test function to use the system clock*/
+void sleep(int micros){
+
+  int curr = *(get_sys_clock()->lo_32bits) + micros;
+
+  while(*(get_sys_clock()->lo_32bits) < curr);
+
+}
 
 
 
@@ -15,19 +26,22 @@
  */
 int gpio_test(struct gpio_pin *pins, int size)
 {
- volatile unsigned int tim;
+ //volatile unsigned int tim;
 
       if((set_gpio_fnct(&pins[18], 1)) == -1)
             return (-1);
 
         while(1)
     {
-        for(tim = 0; tim < 500000; tim++);
+        //for(tim = 0; tim < 500000; tim++);
+          sleep(5000000);
 
            if((send_gpio_sig(&pins[18])) == -1) /*Send a 3.3v signal to pin 18*/
                 return (-1);
 
-          for(tim = 0; tim < 900000; tim++);
+         // for(tim = 0; tim < 900000; tim++);
+
+         sleep(5000000);
 
       if((clear_gpio_sig(&pins[18])) == -1) /*Clear the signal form pin 18*/
             return (-1);
