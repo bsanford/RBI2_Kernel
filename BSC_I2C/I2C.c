ff1e6fe685226abaeb6a9bffda99ecb19db8c693
bbot <sanford.braeden@gmail.com>
2016-12-16 17:18:18 -0500

    Added the beginings of the I2C sources

---
+++ BSC_I2C/I2C.c
@@ -0,0 +1,53 @@
+//
+// Created by bbot on 12/16/16.
+//
+
+/** Implementation of the Broadcom Serial Controller
+ *  Which is Philips I2C compliant bus controller
+ */
+
+
+
+
+/**Function: set_clock_speed
+ * @brief Sets the I2C clock speed
+ *
+ * @arg clk_spd of the device to write to
+ */
+void set_clock_speed(void){
+
+
+}
+
+
+
+/**Function get_dlen
+ * @brief gets the dlen value from the register
+ *        how much data is to be read from the register
+ *
+ * @return returns the short dlen value
+ */
+
+short get_dlen(void) {
+
+
+}
+
+
+/**Function set_dlen
+ * @brief writes the dlen value to the register ie
+ *        how much data will be written on to the i2C line
+ *
+ * @return returns nothing
+ */
+void set_dlen(int length){
+
+
+}
+
+
+
+
+
+
+
